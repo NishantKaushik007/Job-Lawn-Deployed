b0ea3b23a30ff3444b33ae5031b47aaba190aa2e
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-03-06 15:43:01 +0530

    updated next-sitemap.config.js to exclude some paths from robots.txt file also

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,15 @@
 module.exports = {
     siteUrl: 'https://www.joblawn.com',
     generateRobotsTxt: true,
-    exclude: ['/dashboard/*', '/verify-otp/*', '/forget-password/*']
+    exclude: ['/dashboard/*', '/verify-otp/*', '/forget-password/*'],
+    robotsTxtOptions: {
+      policies: [
+        {
+          userAgent: '*',
+          allow: '/',
+          disallow: ['/dashboard/*', '/verify-otp/*', '/forget-password/*']
+        }
+      ]
+    }
   }
+  
