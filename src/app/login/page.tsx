1b278ecaff6602434ed4529276074959ee2be0c3
Nishant Kaushik <nishant.kaushik.pg21@nsut.ac.in>
2025-03-09 02:39:02 +0530

    Extra non used hooks removed from import

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect, useRef } from "react";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
